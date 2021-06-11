942ceee3feb16f8fd26a54b3483a3086b285e7bf
zaytsev <zaytsev@cad.ru>
2021-06-11 09:50:24 +0300

    add comment

--- RicQRCoder/Program.cs
+++ RicQRCoder/Program.cs
@@ -33,7 +33,7 @@ namespace RicQRCoder
                 h.MaximumDisplayWidth = 200;
                 h.Heading = "RicQRCoder 1.0.0"; //change header
                 h.Copyright = "Copyright (c) 2021 cad.ru"; //change copyrigt text
-                h.AddPreOptionsLine("");// ("<<license as is>>");
+                h.AddPreOptionsLine("using two libraries (in the program directory): QRCoder.dll, CommandLine.dll");// ("<<license as is>>");
                 h.AddPostOptionsText("Good luck...");
                 return HelpText.DefaultParsingErrorsHandler(result, h);
             }, e => e);
